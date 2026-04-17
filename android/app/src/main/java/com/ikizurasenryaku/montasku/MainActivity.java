93ebbd9669c82c7ad733f0d68704cc94a61f7a8b
youna78 <54744847+youna78@users.noreply.github.com>
2026-04-17 22:42:38 +0900

    アプリ修正　プラグイン証明

--- android/app/src/main/java/com/ikizurasenryaku/montasku/MainActivity.java
+++ android/app/src/main/java/com/ikizurasenryaku/montasku/MainActivity.java
@@ -1,5 +1,13 @@
 package com.ikizurasenryaku.montasku;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import io.capawesome.capacitorjs.plugins.firebase.authentication.FirebaseAuthenticationPlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    registerPlugin(FirebaseAuthenticationPlugin.class);
+    super.onCreate(savedInstanceState);
+  }
+}
